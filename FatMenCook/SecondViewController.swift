8cd88df9a60ca0356c5a5f6a7ca4a937e35015da
Lesyntheti <guillaume.bayle@utt.fr>
2019-11-27 08:38:50 +0100

    Initial Commit

---
+++ FatMenCook/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FatMenCook
+//
+//  Created by Guillaume Bayle on 27/11/2019.
+//  Copyright © 2019 Guillaume Bayle. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
